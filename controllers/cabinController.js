81e6190f778d6f829fd867ba957d310d34917acd
Rahul Karadwal <rahulkaradwal2000@gmail.com>
2024-06-10 17:10:32 -0400

    fix: update cabin controller and cabin route

---
+++ controllers/cabinController.js
@@ -0,0 +1,17 @@
+const Cabin = require('../models/cabinModel');
+const handler = require('../utils/Handler');
+
+// get all cabins
+exports.getAllCabin = handler.getAll(Cabin);
+
+// get a cabin
+exports.getCabin = handler.getOne(Cabin);
+
+// create cabin
+exports.addCabin = handler.addOne(Cabin);
+
+// Update Cabin
+exports.updateCabin = handler.updateOne(Cabin);
+
+// Delete Cabin
+exports.deleteCabin = handler.deleteOne(Cabin);
