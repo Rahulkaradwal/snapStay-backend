1ead037a5dfcb7375436343431f874cf244d9cbd
Rahul Karadwal <rahulkaradwal2000@gmail.com>
2024-06-12 19:04:28 -0400

    feat: add middleware to create password reset token

--- models/userModel.js
+++ models/userModel.js
@@ -45,6 +45,8 @@ const userSchema = new mongoose.Schema({
     select: false,
   },
   passwordChangedAt: Date,
+  passwordResetToken: String,
+  passwordResetExpires: Date,
 });
 
 // hash the password when user signup
@@ -76,6 +78,18 @@ userSchema.methods.changePasswordAfter = function (jwtTimeStamp) {
     return jwtTimeStamp < changedTimeStamp;
   }
   return false;
+};
+
+// create password Reset Token
+userSchema.methods.createPasswordResetToken = function () {
+  const resetToken = crypto.randomBytes(32).toString('hex');
+  this.passwordResetToken = crypto
+    .createHash('sha256')
+    .update(resetToken)
+    .digest('hex');
+
+  this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
+  return resetToken;
 };
 
 const User = mongoose.model('User', userSchema);
